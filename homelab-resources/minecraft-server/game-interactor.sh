9f68917f7c5b5f5a533f82a72177d72d5e1c14ad
Foxy Hunter <80623140+FoxyHunter7@users.noreply.github.com>
2025-03-17 23:08:52 +0100

    game interactor

---
+++ homelab-resources/minecraft-server/game-interactor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+kick_all() {
+    local reason="$1"
+    tmux send-keys -t minecraft-server "/kick @a $reason" Enter
+}
+
+send_chat_server_msg() {
+    local description="$1"
+    local subtext="$2"
+
+    tmux send-keys -t minecraft-server "/tellraw @p [\"\",{\"text\":\"!! SERVER INFO !!:\",\"bold\":true,\"color\":\"gold\",\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Information & alerts coming directly from the server.\",\"color\":\"white\"}]}},{\"text\":\"\\n$description\",\"color\":\"white\"},{\"text\":\"\\n$subtext\",\"italic\":true,\"color\":\"gray\"}]" Enter
+}
+
+send_actionbar_title() {
+    local text="$1"
+    tmux send-keys -t minecraft-server "/title @a actionbar {\"text\":\"$text\"}" Enter
+}
+
+start_server() {
+    tmux new-session -d -s minecraft-server "/usr/bin/java -Xmx6G -Xms4G -jar /opt/rikocraft/server.jar nogui"
+}
+
+stop_server() {
+    tmux send-keys -t minecraft-server "/stop" Enter
+}
