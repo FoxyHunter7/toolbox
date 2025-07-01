1da5c58252b707355c0287b98072321a59313acb
Foxy Hunter <80623140+FoxyHunter7@users.noreply.github.com>
2025-07-01 09:11:06 +0200

    Update game-interactor.sh

--- homelab-resources/minecraft-server/game-interactor.sh
+++ homelab-resources/minecraft-server/game-interactor.sh
@@ -5,19 +5,19 @@ source "$SCRIPT_DIR/runner-config.sh"
 
 kick_all() {
     local reason="$1"
-    tmux send-keys -t minecraft-server "/kick @a $reason" Enter
+    tmux send-keys -t minecraft-server "kick @a $reason" Enter
 }
 
 send_chat_server_msg() {
     local description="$1"
     local subtext="$2"
 
-    tmux send-keys -t minecraft-server "/tellraw @p [\"\",{\"text\":\"!! SERVER INFO !!:\",\"bold\":true,\"color\":\"gold\",\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Information & alerts coming directly from the server.\",\"color\":\"white\"}]}},{\"text\":\"\\n$description\",\"color\":\"white\"},{\"text\":\"\\n$subtext\",\"italic\":true,\"color\":\"gray\"}]" Enter
+    tmux send-keys -t minecraft-server "tellraw @a [\"\",{\"text\":\"!! SERVER INFO !!:\",\"bold\":true,\"color\":\"gold\",\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"Information & alerts coming directly from the server.\",\"color\":\"white\"}]}},{\"text\":\"\\n$description\",\"color\":\"white\"},{\"text\":\"\\n$subtext\",\"italic\":true,\"color\":\"gray\"}]" Enter
 }
 
 send_actionbar_title() {
     local text="$1"
-    tmux send-keys -t minecraft-server "/title @a actionbar {\"text\":\"$text\"}" Enter
+    tmux send-keys -t minecraft-server "title @a actionbar {\"text\":\"$text\"}" Enter
 }
 
 start_server() {
@@ -25,5 +25,5 @@ start_server() {
 }
 
 stop_server() {
-    tmux send-keys -t minecraft-server "/stop" Enter
+    tmux send-keys -t minecraft-server "stop" Enter
 }
